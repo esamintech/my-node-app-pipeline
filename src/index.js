35bb40f3bdf448c3b880b57b0fc83c790e85df9f
“esamsharf” <esamintech@gmail.com>
2024-07-10 18:32:58 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// src/index.js
+function add(a, b) {
+  return a + b;
+}
+
+module.exports = { add };
---
+++ test/index.test.js
@@ -0,0 +1,6 @@
+// test/index.test.js
+const { add } = require("../src/index");
+
+test("adds 1 + 2 to equal 3", () => {
+  expect(add(1, 2)).toBe(3);
+});
